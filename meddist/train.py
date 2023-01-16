b23b93c916222bbb9f980ff9c869f2c475b801aa
Jabareen <nabil.jabareen@charite.de>
2023-01-16 16:20:19 +0100

    GLIOB-303 Add train and validation split

--- meddist/train.py
+++ meddist/train.py
@@ -7,45 +7,46 @@ from monai.networks.nets import DenseNet
 from torch import nn
 
 
-def train_distance_model(model, optimizer, loss_fn, dataloader, num_epochs):
+def run_epoch(model, loss_fn, dataloader, optimizer=None):
 
-    with wandb.init(project="Meddist"):
-        for epoch in range(num_epochs):
-            for iteration, batch in enumerate(dataloader):
+    mode = "valid" if optimizer is None else "train"
 
-                # Prepare GT
-                bboxes = get_cropped_bboxes(batch["image"], "RandSpatialCropSamples")
-                centers = get_bbox_centers(bboxes)
+    for iteration, batch in enumerate(dataloader):
 
-                gt_dist_mat = torch.cdist(
-                    torch.tensor(centers), torch.tensor(centers), p=2.0
-                ).float()
+        # Prepare GT
+        bboxes = get_cropped_bboxes(batch["image"], "RandSpatialCropSamples")
+        centers = get_bbox_centers(bboxes)
 
-                # Prepare forward pass
-                optimizer.zero_grad()
-                image = batch["image"].to("cuda")
+        gt_dist_mat = torch.cdist(
+            torch.tensor(centers), torch.tensor(centers), p=2.0
+        ).float()
 
-                # Forward pass
-                with torch.autocast(device_type="cuda"):
-                    embeddings: torch.Tensor = model(image)
+        # Prepare forward pass
+        if mode == "train":
+            optimizer.zero_grad()
+        image = batch["image"].to("cuda")
 
-                # Get loss
-                embeddings = embeddings.to("cpu", dtype=torch.float32)
-                pred_dist_mat = torch.cdist(embeddings, embeddings, p=2)
-                loss = loss_fn(pred_dist_mat, gt_dist_mat)
+        # Forward pass
+        with torch.autocast(device_type="cuda"):
+            embeddings: torch.Tensor = model(image)
 
-                # Backward pass and optimization
-                loss.backward()
-                optimizer.step()
+        # Get loss
+        embeddings = embeddings.to("cpu", dtype=torch.float32)
+        pred_dist_mat = torch.cdist(embeddings, embeddings, p=2)
+        loss = loss_fn(pred_dist_mat, gt_dist_mat)
 
-                # Log
-                wandb.log({"Loss": loss.item()})
+        # Backward pass and optimization
+        if mode == "train":
+            loss.backward()
+            optimizer.step()
+
+        # Log
+        wandb.log({f"Loss/{mode}": loss.item()})
+
+    torch.cuda.empty_cache()
 
 
-if __name__ == "__main__":
-
-    # Read config file specifed in command line arguments.
-    init_wandb()
+def run_training():
 
     # Define the model and optimizer
     model = DenseNet(
@@ -61,7 +62,16 @@ if __name__ == "__main__":
         wandb.config.path_to_images, num_samples=wandb.config.number_of_crops
     )
 
-    # Train the model
-    train_distance_model(
-        model, optimizer, loss_fn, train_loader, num_epochs=wandb.config.epochs
-    )
+    # Start training
+    for _ in range(wandb.config.epochs):
+        run_epoch(model, loss_fn, train_loader, optimizer)
+        run_epoch(model, loss_fn, valid_loader)
+
+
+if __name__ == "__main__":
+
+    # Read config file specifed in command line arguments.
+    init_wandb()
+
+    # New training run
+    run_training()
