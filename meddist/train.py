91a3861c80c7348332d5cdf81158adb28fff50af
Jabareen <nabil.jabareen@charite.de>
2023-01-16 11:10:45 +0100

    Add wandb

--- meddist/train.py
+++ meddist/train.py
@@ -1,37 +1,39 @@
 import torch
+import wandb
 from meddist.data import get_dataloaders
-from meddist.dist import get_bbox_centers, get_cropped_bboxes, pairwise_comparisons
+from meddist.dist import get_bbox_centers, get_cropped_bboxes
 from monai.networks.nets import DenseNet
 from torch import nn
 
 
 def train_distance_model(model, optimizer, loss_fn, dataloader, num_epochs):
 
-    for epoch in range(num_epochs):
-        for iteration, batch in enumerate(dataloader):
+    with wandb.init(project="Meddist"):
+        for epoch in range(num_epochs):
+            for iteration, batch in enumerate(dataloader):
 
-            bboxes = get_cropped_bboxes(batch["image"], "RandSpatialCropSamples")
-            centers = get_bbox_centers(bboxes)
+                bboxes = get_cropped_bboxes(batch["image"], "RandSpatialCropSamples")
+                centers = get_bbox_centers(bboxes)
 
-            gt_dist_mat = torch.cdist(
-                torch.tensor(centers), torch.tensor(centers), p=2.0
-            ).float()
+                gt_dist_mat = torch.cdist(
+                    torch.tensor(centers), torch.tensor(centers), p=2.0
+                ).float()
 
-            # Forward pass
-            image = batch["image"].to("cuda")
-            embeddings = model(image).cpu()
+                # Forward pass
+                image = batch["image"].to("cuda")
+                embeddings = model(image).cpu()
 
-            pred_dist_mat = torch.cdist(embeddings, embeddings, p=2)
+                pred_dist_mat = torch.cdist(embeddings, embeddings, p=2)
 
-            loss = loss_fn(pred_dist_mat, gt_dist_mat)
+                loss = loss_fn(pred_dist_mat, gt_dist_mat)
 
-            # Backward pass and optimization
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                # Backward pass and optimization
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-            # Log
-            print(f"Epoch/Iteration {epoch:03}/{iteration:03} Loss, {loss.item():.3f}")
+                # Log
+                wandb.log({"Loss": loss.item()})
 
 
 # Define the model and optimizer
@@ -42,7 +44,7 @@ optimizer = torch.optim.Adam(model.parameters())
 loss_fn = nn.MSELoss()
 
 # Define the DataLoader
-data_path = "/sc-scratch/sc-scratch-gbm-radiomics/tcia/manifest-1654187277763/nifti/FDG-PET-CT-Lesions-data2"
+data_path = "/sc-scratch/sc-scratch-gbm-radiomics/tcia/manifest-1654187277763/nifti/FDG-PET-CT-Lesions-data2/registered"
 train_loader, valid_loader = get_dataloaders(data_path)
 
 # Train the model
