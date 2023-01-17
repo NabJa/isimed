f6b00b3f28e358e2103c18b3592ca9f13dc684ca
Jabareen <nabil.jabareen@charite.de>
2023-01-17 14:32:39 +0100

    GLIOB-304 Add model checkpoint saving #done

--- meddist/train.py
+++ meddist/train.py
@@ -1,3 +1,7 @@
+import logging
+from pathlib import Path
+
+import numpy as np
 import torch
 import wandb
 from meddist.config import init_wandb
@@ -5,6 +9,62 @@ from meddist.data import get_dataloaders
 from meddist.dist import get_bbox_centers, get_cropped_bboxes
 from monai.networks.nets import DenseNet
 from torch import nn
+
+
+class CheckpointSaver:
+    def __init__(self, dirpath, decreasing=True, top_n=5):
+        """
+        dirpath: Directory path where to store all model weights
+        decreasing: If decreasing is `True`, then lower metric is better
+        top_n: Total number of models to track based on validation metric value
+        """
+
+        self.dirpath = Path(dirpath)
+        self.dirpath.mkdir(parents=True, exist_ok=True)
+
+        self.top_n = top_n
+        self.decreasing = decreasing
+        self.top_model_paths = []
+        self.best_metric_val = np.Inf if decreasing else -np.Inf
+
+    def __call__(self, model, epoch, metric_val):
+        model_path = self.dirpath / (model.__class__.__name__ + f"_epoch{epoch:03}.pt")
+        save = (
+            metric_val < self.best_metric_val
+            if self.decreasing
+            else metric_val > self.best_metric_val
+        )
+
+        if save:
+            logging.info(
+                f"Current metric value better than {metric_val} better than best {self.best_metric_val}, saving model at {model_path}, & logging model weights to W&B."
+            )
+            self.best_metric_val = metric_val
+            torch.save(model.state_dict(), model_path)
+            self.log_artifact(f"model-ckpt-epoch-{epoch}.pt", model_path, metric_val)
+            self.top_model_paths.append({"path": model_path, "score": metric_val})
+            self.top_model_paths = sorted(
+                self.top_model_paths,
+                key=lambda o: o["score"],
+                reverse=not self.decreasing,
+            )
+
+        if len(self.top_model_paths) > self.top_n:
+            self.cleanup()
+
+    def log_artifact(self, filename, model_path, metric_val):
+        artifact = wandb.Artifact(
+            filename, type="model", metadata={"Validation score": metric_val}
+        )
+        artifact.add_file(model_path)
+        wandb.run.log_artifact(artifact)
+
+    def cleanup(self):
+        to_remove = self.top_model_paths[self.top_n :]
+        logging.info(f"Removing extra models.. {to_remove}")
+        for o in to_remove:
+            Path(o["path"]).unlink()
+        self.top_model_paths = self.top_model_paths[: self.top_n]
 
 
 def get_max_distance(dist_mat1: torch.Tensor, dist_mat2: torch.Tensor) -> float:
@@ -60,13 +120,18 @@ def run_epoch(model, loss_fn, dataloader, optimizer=None) -> None:
             running_max_dist if running_max_dist > max_distance else max_distance
         )
 
+        if iteration == 5:
+            break
+
     # Free up all memory
     torch.cuda.empty_cache()
 
+    epoch_loss = running_loss / iteration
+
     if mode == "valid":
-        wandb.log(
-            {f"{mode}/Loss": loss.item(), f"{mode}/MaxDistance": running_max_dist}
-        )
+        wandb.log({f"{mode}/Loss": epoch_loss, f"{mode}/MaxDistance": running_max_dist})
+
+    return epoch_loss
 
 
 def run_training():
@@ -82,15 +147,24 @@ def run_training():
 
     # Define the DataLoader
     train_loader, valid_loader = get_dataloaders(
-        wandb.config.path_to_images, num_samples=wandb.config.number_of_crops, add_intensity_augmentation=wandb.config.augment
+        wandb.config.path_to_images,
+        num_samples=wandb.config.number_of_crops,
+        add_intensity_augmentation=wandb.config.augment,
     )
 
+    # Define checkpoint saver
+    saver = CheckpointSaver(wandb.config.output_path, decreasing=True, top_n=1)
+
     # Start training
-    for _ in range(wandb.config.epochs):
-        run_epoch(model, loss_fn, train_loader, optimizer)
+    for epoch in range(wandb.config.epochs):
+        _ = run_epoch(model, loss_fn, train_loader, optimizer)
 
         with torch.no_grad():
-            run_epoch(model, loss_fn, valid_loader)
+            valid_loss = run_epoch(model, loss_fn, valid_loader)
+
+        # Save checkpoints only 30% into the training. This prevents saving to early models.
+        if epoch > wandb.config.epochs * 0.3:
+            saver(model, epoch, valid_loss)
 
 
 if __name__ == "__main__":
