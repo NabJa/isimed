75cd0e8b84087d05f8dec790ae944332830eeabe
Jabareen <nabil.jabareen@charite.de>
2023-02-13 09:34:51 +0100

    GLIOB-318 Prepare config for sweep

--- meddist/config.py
+++ meddist/config.py
@@ -1,6 +1,14 @@
 import argparse
 
 import wandb
+import yaml
+
+ARGUMENTS = [("data", str), ("model", str), ("crop_size", int)]
+
+
+def read_yaml(path):
+    with open(path, mode="r") as file:
+        return yaml.safe_load(file)
 
 
 def read_args():
@@ -9,11 +17,20 @@ def read_args():
         "-config",
         type=str,
         help="Path to config file.",
-        default="/sc-projects/sc-proj-gbm-radiomics/whole-body/meddistssl/configs/simple.yaml",
+        default="/sc-projects/sc-proj-gbm-radiomics/whole-body/meddistssl/configs/train.yaml",
     )
+    for arg, arg_type in ARGUMENTS:
+        parser.add_argument(f"--{arg}", type=arg_type)
+
     return parser.parse_args()
 
 
 def init_wandb(project_name="Meddist"):
     args = read_args()
+
     wandb.init(project=project_name, config=args.config)
+
+    for arg, arg_type in ARGUMENTS:
+        argument = args.__getattribute__(arg)
+        if argument is not None:
+            wandb.config.update({arg: arg_type(argument)})
