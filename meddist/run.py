68d77b245bf43c54f3f78dc432a6cc2f3517195e
Jabareen <nabil.jabareen@charite.de>
2023-02-14 10:12:34 +0100

    Compute number of crops based on batch size

--- meddist/run.py
+++ meddist/run.py
@@ -23,15 +23,25 @@ def cleanup_model_log_directory(root):
             os.rmdir(path)
 
 
+def get_ncrops(
+    batch_size, crop_size, num_allowed_voxels=50_000_000, min_crops=1, max_crops=512
+) -> int:
+    """Compute the number of crops for a given batch_size and crop_size.
+    Returns the number of crops so that ncrops * batch_size * crop_size**3 < num_allowed_voxels."""
+    ncrops = num_allowed_voxels / (batch_size * crop_size ** 3)
+    ncrops = int((ncrops // 8) * 8)  # Make ncrops divisable by 8
+    return min(max(ncrops, min_crops), max_crops)
+
+
 def parse_wandb_config() -> Tuple[str, str, callable]:
 
-    model_log_path = MODEL_LOG_ROOT / f"{wandb.config.model}_{wandb.config.data}/{wandb.run.name}_{wandb.run.id}"
+    model_log_path = (
+        MODEL_LOG_ROOT
+        / f"{wandb.config.model}_{wandb.config.data}/{wandb.run.name}_{wandb.run.id}"
+    )
 
-    n_crops = 16384 / (wandb.config.crop_size * wandb.config.batch_size)
-    n_crops = max(int(n_crops), 1)
-
-    if n_crops < wandb.config.number_of_crops:
-        wandb.config.update({"number_of_crops": n_crops}, allow_val_change=True)
+    ncrops = get_ncrops(wandb.config.batch_size, wandb.config.crop_size)
+    wandb.config.update({"number_of_crops": ncrops}, allow_val_change=True)
 
     return model_log_path
 
@@ -40,7 +50,7 @@ if __name__ == "__main__":
 
     set_determinism()
 
-    with init_wandb(project_name="Meddist-test"):
+    with init_wandb(project_name="simCLR"):
         model_log_path = parse_wandb_config()
         path_to_data_split = DATA_PATHS[wandb.config.data]
         train(path_to_data_split, model_log_path)
