06b56fe76867b4c704d4187ca3d5eceb71b44dd1
Jabareen <nabil.jabareen@charite.de>
2023-02-24 15:44:25 +0100

    Move metric tracking functionality to own file

--- meddist/downstream.py
+++ meddist/downstream.py
@@ -1,20 +1,15 @@
 import argparse
-from abc import abstractmethod
 from typing import List, Tuple
 
 import numpy as np
 import pandas as pd
 import torch
-from monai.metrics import (
-    compute_confusion_matrix_metric,
-    compute_roc_auc,
-    get_confusion_matrix,
-)
 from monai.utils.misc import set_determinism
 from torch import nn
 from tqdm import tqdm
 
 from meddist.data.loading import kfold_get_downstram_classification_data
+from meddist.metrics import ClassificationMetricTracker, RegressionMetricTracker
 from meddist.nets import LinearHead, load_latest_densenet
 
 torch.multiprocessing.set_sharing_strategy("file_system")
@@ -22,79 +17,6 @@ set_determinism()
 
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-
-
-class Cumulative:
-    def __init__(self) -> None:
-        self.reset()
-
-    def __call__(self, y_pred, y) -> None:
-        """Add batch first data to buffers."""
-        self.pred_buffer.append(y_pred)
-        self.label_buffer.append(y)
-
-    def reset(self):
-        """Init/Empty buffers"""
-        self.pred_buffer = []
-        self.label_buffer = []
-
-    @abstractmethod
-    def aggregate(self, *args, **kwargs):
-        """
-        Aggregate final results based on the gathered buffers.
-        Computation of metrics based on labels and prediction must be done in this method.
-
-        """
-        raise NotImplementedError(
-            f"Subclass {self.__class__.__name__} must implement this method."
-        )
-
-
-class RegressionMetricTracker(Cumulative):
-    def __init__(self) -> None:
-        super().__init__()
-
-    def aggregate(self):
-
-        preds = torch.cat(self.pred_buffer, dim=0)
-        labels = torch.cat(self.label_buffer, dim=0)
-
-        rmse = torch.sqrt(torch.pow(preds - labels, 2).mean()).item()
-        mae = torch.abs(preds - labels).mean().item()
-
-        return {"RMSE": rmse, "MAE": mae}
-
-
-class ClassificationMetricTracker(Cumulative):
-    def __init__(
-        self,
-        threshold=0.5,
-        metric_names=("sensitivity", "specificity", "accuracy", "f1 score"),
-    ) -> None:
-        super().__init__()
-        self.threshold = threshold
-        self.metric_names = metric_names
-        self.sigmoid = nn.Sigmoid()
-
-    def aggregate(self):
-
-        preds = torch.cat(self.pred_buffer, dim=0)
-        labels = torch.cat(self.label_buffer, dim=0)
-
-        result = {"AUC": compute_roc_auc(preds, labels)}
-
-        pred_binary = (self.sigmoid(preds) > self.threshold).float()
-        cm = get_confusion_matrix(pred_binary, labels)
-        cm = torch.sum(cm, 0)[0]  # Sum over all batches.
-
-        for name, value in zip(["TP", "FP", "TN", "FN"], cm):
-            result[name] = value.item()
-
-        for name in self.metric_names:
-            result[name] = compute_confusion_matrix_metric(name, cm).item()
-
-        return result
-
 
 TASK_PREP = {
     "classification": (
